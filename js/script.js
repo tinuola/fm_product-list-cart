9f21dc219b0604d7d54fdf50c41a9f766e5d682e
tinuola <24995224+tinuola@users.noreply.github.com>
2024-09-27 02:02:01 -0700

    wip: test basic dialog/modal functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log('Hello!')
+
+const dialog = document.querySelector('dialog')
+
+const confirmBtn = document.querySelector('#confirm-order-btn')
+
+const newOrderBtn = document.querySelector('#new-order-btn')
+
+confirmBtn.addEventListener('click', () => {
+  dialog.showModal()
+})
+
+newOrderBtn.addEventListener('click', () => {
+  dialog.close()
+})
