8c35238443ee63fb11e5bcff3afa6c8d976187db
tinuola <24995224+tinuola@users.noreply.github.com>
2024-09-28 17:04:12 -0700

    wip: basic item selection and checkout display

--- js/script.js
+++ js/script.js
@@ -1,15 +1,63 @@
 import products from '../data/data.js'
 
-const { reactive, toRefs } = Vue
+const { computed, ref, reactive, toRefs } = Vue
 
 const App = {
+  props: {},
+
   setup() {
     const state = reactive({
       products,
+      productRefs: [],
     })
+
+    const productRefs = ref([])
+
+    const selectedItemsList = ref([])
+
+    const totalCount = ref(0)
+
+    const totalOrderPrice = computed(() => {
+      if (!selectedItemsList.value) {
+        return 0
+      }
+
+      const calc = selectedItemsList.value.reduce((a, b) => {
+        return a + b.totalItemPrice
+      }, 0)
+
+      return calc.toFixed(2)
+    })
+
+    // Methods
+    function addToCart(el) {
+      totalCount.value++
+
+      const { name, price, image } = products[el]
+      const { thumbnail } = image
+
+      const selectedItem = {
+        name,
+        price,
+        thumbnail,
+        selected: true,
+        quantity: 1,
+        totalItemPrice: price,
+      }
+
+      selectedItemsList.value.push(selectedItem)
+    }
+
+    function calculateTotal() {}
 
     return {
       ...toRefs(state),
+      addToCart,
+      calculateTotal,
+      productRefs,
+      selectedItemsList,
+      totalCount,
+      totalOrderPrice,
     }
   },
 }
