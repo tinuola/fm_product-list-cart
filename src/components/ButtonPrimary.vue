376ca6e6624781c6011f4594cfc1b2e0409977e9
tinuola <24995224+tinuola@users.noreply.github.com>
2024-10-04 23:22:31 -0700

    feat: basic ButtonPrimary component

---
+++ src/components/ButtonPrimary.vue
@@ -0,0 +1,18 @@
+<script setup>
+const { title } = defineProps({
+  title: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <button>
+    <!-- Icon slot -->
+    <slot />
+    {{ title }}
+  </button>
+</template>
+
+<style lang="scss" scoped></style>
