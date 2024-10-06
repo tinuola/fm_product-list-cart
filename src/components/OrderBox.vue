46ec1909238701528f2491a05eb4a414dd8ad54d
tinuola <24995224+tinuola@users.noreply.github.com>
2024-10-05 19:49:38 -0700

    wip: setup OrderBox component

---
+++ src/components/OrderBox.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div>
+    <slot name="header"></slot>
+
+    <slot name="body"></slot>
+
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
