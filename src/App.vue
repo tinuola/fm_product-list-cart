8c944263d8222ee6930bea2a068df12c89f3e12f
tinuola <24995224+tinuola@users.noreply.github.com>
2024-10-05 21:29:51 -0700

    refactor: create modal component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ import { useHead } from '@unhead/vue'
 import { useProductStore } from '@/stores/ProductStore'
 
 // Components
+import ButtonPrimary from '@/components/ButtonPrimary.vue'
+import ModalBox from '@/components/ModalBox.vue'
 import OrderBox from '@/components/OrderBox.vue'
 import ProductCard from '@/components/ProductCard.vue'
 import SelectedProductList from '@/components/SelectedProductList.vue'
@@ -145,29 +147,7 @@ useHead({
       </div>
 
       <!-- Confirmation Modal -->
-      <dialog class="confirmation-modal" ref="dialog-ref">
-        <OrderBox>
-          <template #header>
-            <div class="confirmation-icon-wrapper">
-              <img src="/images/icon-order-confirmed.svg" alt="Order confirmed icon" />
-            </div>
-            <h3>Order Confirmed</h3>
-            <p>We hope you enjoy your food!</p>
-          </template>
-          <template #body>
-            <SelectedProductList :is-confirmed="true" />
-          </template>
-          <template #footer>
-            <ButtonPrimary
-              class="btn btn--lg btn__new-order u-font-medium"
-              id="confirm-order-btn"
-              @click="startNewOrder"
-            >
-              Start New Order
-            </ButtonPrimary>
-          </template>
-        </OrderBox>
-      </dialog>
+      <ModalBox ref="dialog-ref" @new-order="startNewOrder" />
     </main>
 
     <footer></footer>
---
+++ src/components/ModalBox.vue
@@ -0,0 +1,36 @@
+<script setup>
+import ButtonPrimary from '@/components/ButtonPrimary.vue'
+import OrderBox from '@/components/OrderBox.vue'
+import SelectedProductList from '@/components/SelectedProductList.vue'
+
+defineEmits(['newOrder'])
+</script>
+
+<!--  -->
+<template>
+  <dialog class="confirmation-modal">
+    <OrderBox>
+      <template #header>
+        <div class="confirmation-icon-wrapper">
+          <img src="/images/icon-order-confirmed.svg" alt="Order confirmed icon" />
+        </div>
+        <h3>Order Confirmed</h3>
+        <p>We hope you enjoy your food!</p>
+      </template>
+      <template #body>
+        <SelectedProductList :is-confirmed="true" />
+      </template>
+      <template #footer>
+        <ButtonPrimary
+          class="btn btn--lg btn__new-order u-font-medium"
+          id="confirm-order-btn"
+          @click="$emit('newOrder')"
+        >
+          Start New Order
+        </ButtonPrimary>
+      </template>
+    </OrderBox>
+  </dialog>
+</template>
+
+<style lang="scss" scoped></style>
